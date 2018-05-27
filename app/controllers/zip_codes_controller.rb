28eb2c5b065fda1ae49deab3354bc8d78368e970
nizoraul <nizoraul@gmail.com>
2018-05-27 22:43:54 +0900

    秋田の検索フォームのトップページを実装

---
+++ app/controllers/zip_codes_controller.rb
@@ -0,0 +1,6 @@
+class ZipCodesController < ApplicationController
+  def index
+    @q = ZipCode.ransack(params[:q])
+    @zip_codes = @q.result(distinct: true)
+  end
+end
--- app/models/zip_code.rb
+++ app/models/zip_code.rb
@@ -1,2 +1,6 @@
 class ZipCode < ApplicationRecord
+  # 住所.jpから取得したcsvをpostgreSQLのCOPYでインポートするコマンド Herokuの仕様で10000レコード制限があるので全国は取り込めない。
+  # COPY zip_codes (address_code, prefecture_code, city_code, town_code, zip_code, office_flag, abolition_flag, prefecture, prefecture_kana, city, city_kana, town, town_kana, town_supplement, kyoto_street_name, section, section_kana, supplement, office, office_kana, office_address, new_address_code, created_at, updated_at) from 'ップ/05akita.csv' WITH csv HEADER;
+  # とりあえずお試しで秋田のみ取り込み
+  # cp932なのでiconvとかでutf8に変更してから取り込む。
 end
